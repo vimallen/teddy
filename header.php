3138b4b247166556e9fb0d8afa81463b3ff49144
David Allen <mrdavidrobertallen@gmail.com>
2024-09-08 21:12:37 +0100

    Removed small menu

--- header.php
+++ header.php
@@ -46,8 +46,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'teddy' ); ?></button>
-			<?php
+					<?php
 			wp_nav_menu(
 				array(
 					'theme_location' => 'menu-1',
