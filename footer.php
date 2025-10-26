a3515a504fa85725468cb697053f0c24b833e2d5
David Allen <mrdavidrobertallen@gmail.com>
2025-10-26 21:56:49 +0000

    Looking at the footer

--- footer.php
+++ footer.php
@@ -11,23 +11,12 @@
 
 ?>
 
+		</div><!-- #content -->
+
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'teddy' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'teddy' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'teddy' ), 'teddy', '<a href="https://www.daveallengraphics.co">Dave Allen</a>' );
-				?>
-		</div><!-- .site-info -->
+	<p>&copy; Dave Allen Graphics <?php echo date("Y"); ?></p>	
 	</footer><!-- #colophon -->
 </div><!-- #page -->
-
 <?php wp_footer(); ?>
 
 </body>
--- header.php
+++ header.php
@@ -35,7 +35,7 @@
 				<?php
 			else :
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 				<?php
 			endif;
 			$teddy_description = get_bloginfo( 'description', 'display' );
