7bd58dce187cb1a85b197348318072264c677801
David Allen <mrdavidrobertallen@gmail.com>
2025-10-26 12:40:11 +0000

    Moved search to top of blog page, Control is via wpadmin

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
  */
 
 get_header();
+get_sidebar();
 ?>
 
 	<main id="primary" class="site-main">
@@ -53,5 +54,5 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
+/* get_sidebar(); */
 get_footer();
